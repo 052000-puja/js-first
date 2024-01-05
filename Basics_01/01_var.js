607b2fbe4fb2314e7d1639cdc27981a727a4bad1
Ercel <78751475+052000-puja@users.noreply.github.com>
2024-01-05 20:15:59 +0000

    container config

---
+++ Basics_01/01_var.js
@@ -0,0 +1,14 @@
+const accountId = 12345
+let accountEmail = "Puja@gmail.com"
+var accountPassword= "12345"
+accountCity = "Jharkhand"
+
+// accountId = 2 // not allowed
+
+accountEmail = "Rani@gmail.com"
+accountPassword= "123456"
+accountCity = "Bihar"
+
+console.log(accountId);
+
+console.table([accountEmail, accountId, accountCity, accountPassword])
